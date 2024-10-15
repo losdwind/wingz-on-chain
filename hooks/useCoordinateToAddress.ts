e8ff98bff7c715c84da417afb22d713d0bc5206d
losdwind <s524256521@gmail.com>
2024-10-16 04:54:01 +0800

    refactor: extract fetchAddress as function

--- hooks/useCoordinateToAddress.ts
+++ hooks/useCoordinateToAddress.ts
@@ -5,25 +5,26 @@ import * as Location from 'expo-location';
 const useCoordinateToAddress = (location: LatLng, forceFetch?: boolean) => {
   const [address, setAddress] =
     useState<Location.LocationGeocodedAddress | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      if (location.latitude && location.longitude) {
-        try {
-          const [resAddress] = await Location.reverseGeocodeAsync({
-            latitude: location.latitude,
-            longitude: location.longitude,
-          });
-          setAddress(resAddress || null);
-        } catch (error) {
-          if (!forceFetch) {
-            setAddress(null);
-          }
-          console.log(error);
+  const fetchAddress = async (location: LatLng) => {
+    if (location.latitude && location.longitude) {
+      try {
+        const [resAddress] = await Location.reverseGeocodeAsync({
+          latitude: location.latitude,
+          longitude: location.longitude,
+        });
+        setAddress(resAddress || null);
+      } catch (error) {
+        if (!forceFetch) {
+          setAddress(null);
         }
+        console.log(error);
       }
-    })();
-  }, [location, forceFetch]);
+    }
+  };
+  useEffect(() => {
+    fetchAddress(location);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
 
   return address;
 };
