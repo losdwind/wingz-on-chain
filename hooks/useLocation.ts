88b8c7e56b5815ceb2a4a6a116f96373dec4b6e2
losdwind <s524256521@gmail.com>
2024-10-16 04:51:46 +0800

    fix: isloading missing for hook

--- hooks/useLocation.ts
+++ hooks/useLocation.ts
@@ -4,29 +4,39 @@ import { Region } from 'react-native-maps';
 
 export default function useLocation() {
   const [region, setRegion] = useState<Region>();
-
+  const [isLoading, setIsLoading] = useState(true);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
   useEffect(() => {
     const getLocation = async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
+      setIsLoading(true);
+      try {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+
+        setRegion({
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.05,
+          longitudeDelta: 0.05,
+        });
+      } catch (error) {
+        // setErrorMsg(
+        //   'Failed to get current location. Please ensure location services are enabled.'
+        // );
+        // console.error('Error getting location:', error);
+      } finally {
+        setIsLoading(false);
       }
-
-      let location = await Location.getCurrentPositionAsync({});
-
-      setRegion({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 0.05,
-        longitudeDelta: 0.05,
-      });
     };
 
     getLocation();
   }, []);
 
-  return { region, setRegion, errorMsg, setErrorMsg };
+  return { region, setRegion, errorMsg, setErrorMsg, isLoading };
 }
